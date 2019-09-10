1160d03c7cb0b03d2199d60dd895cd6e797cba13
Volodymyr <31970034+datsikvv@users.noreply.github.com>
2019-09-10 07:42:51 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,27 +14,167 @@ let createTable = () => {
     $('body').append(container);   
 };
 
- function runRightInterval(timer) {
+//  function runRightInterval(timer) {
 
-    let gameInterval = setInterval(() => {
-        $(".ball").offset({top: pos.top + 10, left: pos.left + 10})
-        pos = $(".ball").offset();
+//     let gameInterval = setInterval(() => {
+//         $(".ball").offset({top: pos.top + 10, left: pos.left + 10})
+//         pos = $(".ball").offset();
+//         if (pos.left === bord) {
+//             console.log(pos);
+//             console.log(bord);
+//             console.log(startPosit);
+
+
+//             // runLeftInterval(timer)
+//         }
+//     }, timer);
+// };
+
+// function runLeftInterval(timer) {
+//     let gameInterval = setInterval(() => {
+//         $(".ball").offset({top: pos.top + 10, left: pos.left - 10})
+//         pos = $(".ball").offset();
+//         console.log("test " + pos )
+//         // if (pos.left === startPosit) {
+//         //     runRightInterval(timer)
+//         // }
+//     }, timer);
+// };
+
+let direct;
+
+// function runInterval(timer) {
+
+//     if (direct === true) {
+//         let gameInterval = setInterval( () => {
+//             $(".ball").offset({top: pos.top + 10, left: pos.left + 10})
+//             pos = $(".ball").offset();
+
+//     }, timer);
+//     if (Math.floor( pos.left) === Math.floor( bord)) {
+//         direct = false;
+//         return
+//     }
+//     } else if (direct === false) {
+//         let gameInterval = setInterval(() => {
+//             $(".ball").offset({top: pos.top + 10, left: pos.left - 10})
+//             pos = $(".ball").offset();
+
+//     }, timer);
+//     if (Math.floor( pos.left) === Math.floor( startPosit )) {
+//         direct = true;
+//         return
+//     }
+//     }
+// };
+
+function runIntervalStart(timer) {
+        if (pos.left > 10) {
+            direct = true;
+        }
+    
+        let gameInterval = setInterval( () => {
+            $(".ball").offset({top: pos.top + 10, left: pos.left + 10})
+            pos = $(".ball").offset();
+            if (Math.floor( pos.left) === Math.floor( bord)) {
+                direct = false;
+                runLeftInterval(timer); 
+            }
     }, timer);
+    
+
+
+};
+
+
+function runRightInterval(timer) {
+
+    // do {
+    //     let gameInterval = setInterval(() => {
+    //                 $(".ball").offset({top: pos.top + 10, left: pos.left + 10})
+    //                 pos = $(".ball").offset();
+    //             }, timer);
+    // } while (Math.floor( pos.left) != Math.floor( bord) );
+    
+    // direct = false;
+    // runLeftInterval(timer)
+    for ( let i = pos.left; direct === true; ) {
+        let gameInterval = setInterval(() => {
+            $(".ball").offset({top: pos.top + 10, left: pos.left + 10})
+            pos = $(".ball").offset();
+        }, timer);
+        if (Math.floor( pos.left) === Math.floor( bord)) {
+            direct = false;
+         
+        }
+    }
+    return
+
+
+
 };
 
 function runLeftInterval(timer) {
-    let gameInterval = setInterval(() => {
-        $(".ball").offset({top: pos.top + 10, left: pos.left - 10})
-        pos = $(".ball").offset();
-    }, timer);
+
+    // do {
+    //     let gameInterval = setInterval(() => {
+    //                 $(".ball").offset({top: pos.top + 10, left: pos.left - 10})
+    //                 pos = $(".ball").offset();
+    //             }, timer);
+    // } while (Math.floor( pos.left) != Math.floor( startPosit ) );
+    
+    // direct = true;
+    // runRightInterval(timer)
+    
+    for (let i = pos.left; direct === false; ) {
+        console.log( "xxxxxxxxxx")
+        setInterval(() => {
+            console.log( "zzzzzzzzzzz")
+            $(".ball").offset({top: pos.top + 10, left: pos.left - 10})
+            console.log( pos.left)
+            pos = $(".ball").offset();
+        }, timer);
+        if (Math.floor( pos.left) === Math.floor( startPosit)) {
+            direct = true;
+           
+        }
+    }
+
+    return 
 };
+
+
+
+
 
 createTable();
 let pos = $(".ball").offset();
-console.log(pos)
+const bord = pos.left + 400;
+const startPosit =  pos.left;
+console.log(pos);
+console.log(startPosit);
+console.log(bord);
 
-if (pos.left < 500) {
-    runRightInterval(timer);
-} else if (pos.left > 0) {
- runLeftInterval(timer);
-}
+runIntervalStart(timer);
+
+// for (let i = pos.left; i < bord; i += 10) {
+//     // if (i === 3) { continue; }
+//     // text += "The number is " + i + "<br>";
+//     runRightInterval(timer);
+//     console.log(i)
+//   }
+
+// if (pos.left < bord) {
+//     runRightInterval(timer);
+// } else if (pos.left > bord) {
+//  runLeftInterval(timer);
+// }
+// for (let i = pos.left; i < bord; ) {
+//     console.log(i +"x")
+//     if (i < bord) {  
+//          runRightInterval(timer)
+//     } else if  (i > startPosit) {  
+//         runLeftInterval(timer)
+//    }
+ 
+//   }
